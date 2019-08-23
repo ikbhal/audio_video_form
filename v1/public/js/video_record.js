f9fdfe1af8f92af09a16e3c7ab873560de1dc17f
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2019-08-23 15:56:48 +0530

    adding button to record, stop

--- v1/public/js/video_record.js
+++ v1/public/js/video_record.js
@@ -1,18 +1,6 @@
 
-var fileData = null;
-
-function loadFile() {
-    var preview = document.querySelector('file');
-    var file    = document.querySelector('input[type=file]').files[0];
-    var reader  = new FileReader();
-
-    reader.onloadend = function () {
-        fileData = file;
-    }
-    if (file) {
-        reader.readAsDataURL(file);
-    }
-}
+var stream;
+var recorder;
 
 function uploadFile(blob) {
     data = new FormData();
@@ -33,20 +21,22 @@ function uploadFile(blob) {
         }
     });
 }
-async function record_video() {
-    let stream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
-    let recorder = new RecordRTCPromisesHandler(stream, {
+async function recordVideo() {
+    stream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
+    recorder = new RecordRTCPromisesHandler(stream, {
         type: 'video'
     });
     recorder.startRecording();
-
-    const sleep = m => new Promise(r => setTimeout(r, m));
-    await sleep(3000);
-
+}
+async function stopRecordingVideo(){
     await recorder.stopRecording();
     let blob = await recorder.getBlob();
-    //invokeSaveAsDialog(blob);
     await uploadFile(blob);
 }
 
-record_video();
+//record_video();
+$(document).ready(function(){
+    $("#record_button").click(recordVideo);
+
+    $("#stop_record_button").click(stopRecordingVideo);
+});
